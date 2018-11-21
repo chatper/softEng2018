55d8034e4dd40b1d66082e62992e9dbc26b3d66d
fotini-pan <fotinipan97@gmail.com>
2018-11-21 16:02:12 +0200

    App Initialization: Changed API

--- app/controllers/api/users_controller.py
+++ app/controllers/api/users_controller.py
@@ -1,16 +1,18 @@
-#Αυτά είναι μια αρχική ιδέα, αλλά βασικά πρέπει να δω να χρησιμοποιήσω το flask_security
+# Αυτά είναι μια αρχική ιδέα, αλλά βασικά πρέπει να δω να χρησιμοποιήσω το flask_security
 
 from flask import Flask, request, make_response, jsonify
 from app import app, db
 from models.user import User
 #from AggelikisFile import AggelikisClass
-#Beware of circular dependencies duuuudes!
+
 
 @app.route('/api/v1.0/users', methods=['GET'])
 def home():
     q = User.query.all()
-    users = [{'email': u.email, 'password': u.password} for u in q]
-    return make_response(jsonify(users), 200)
+    #users = [{'email': u.email, 'password': u.password} for u in q]
+    users = [u.serialize for u in q]
+    ret = [{key: u[key] for key in ['email', 'password']} for u in users]
+    return make_response(jsonify(ret), 200)
 
 
 @app.route('/api/v1.0/users', methods=['POST'])
@@ -24,18 +26,20 @@ def register():
     return make_response(jsonify({}), 201)
 
 
-@app.route('/api/v1.0/users/<user_email>', methods=['GET'])
-def profile(user_email):
-    q = User.query.filter_by(email=user_email).first()
+@app.route('/api/v1.0/users/<user_id>', methods=['GET'])
+def profile(user_id):
+    q = User.query.filter_by(id=user_id).first()
     if q is None:
         return make_response(jsonify({'error': 'Not found'}), 404)
-    existing_user = {'email': q.email, 'password': q.password}
-    return make_response(jsonify(existing_user), 200)
+    #existing_user = {'email': q.email, 'password': q.password}
+    existing_user = q.serialize
+    ret = {key: existing_user[key] for key in ['email', 'password']}
+    return make_response(jsonify(ret), 200)
 
 
-@app.route('/api/v1.0/users/<user_email>', methods=['DELETE'])
-def delete(user_email):
-    existing_user = User.query.filter_by(email=user_email).first()
+@app.route('/api/v1.0/users/<user_id>', methods=['DELETE'])
+def delete(user_id):
+    existing_user = User.query.filter_by(id=user_id).first()
     if existing_user is None:
         return make_response(jsonify({'error': 'Not found'}), 404)
     db.session.delete(existing_user)
@@ -43,9 +47,9 @@ def delete(user_email):
     return make_response(jsonify({}), 200)
 
 
-@app.route('/api/v1.0/users/<user_email>', methods=['PUT'])
-def updatePassword(user_email):
-    existing_user = User.query.filter_by(email=user_email).first()
+@app.route('/api/v1.0/users/<user_id>', methods=['PUT'])
+def updatePassword(user_id):
+    existing_user = User.query.filter_by(id=user_id).first()
     if existing_user is None:
         return make_response(jsonify({'error': 'Not found'}), 404)
     existing_user.password = request.json['password']
@@ -55,7 +59,8 @@ def updatePassword(user_email):
 
 @app.route('/api/v1.0/users/login', methods=['POST'])
 def login():
-    existing_user = User.query.filter_by(email=request.json['email'], password=request.json['password']).first()
+    existing_user = User.query.filter_by(
+        email=request.json['email'], password=request.json['password']).first()
     if existing_user is None:
         return make_response(jsonify({'error': 'Not found'}), 404)
     return make_response(jsonify({}), 200)
--- app/models/user.py
+++ app/models/user.py
@@ -2,15 +2,22 @@ from app import db
 
 
 # Xρησιμοποίησα την class User για δοκιμές.
-# Στο app.py αντί για from models.user import User
+# Στο users_controller.py αντί για from models.user import User
 # κάντε import την κλάση της Αγγελικής από το αρχείο της.
-# Θεώρησα ότι στη βάση δε γίνεται να υπάρχουν δύο rows με ίδια τιμή για το column email
-# οπότε θα μπορούσε αυτό να είναι primary key και το id περιττο (;;;)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
+    @property
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.user
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+            'password': self.password
+        }
